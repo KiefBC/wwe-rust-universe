d73e2141c6020d849aaf6ffdd14a0e026dbd7fe7
kiefer <kiefx@pm.me>
2024-07-22 20:17:13 -0700

    Code formatting.

--- migrations/2024-07-19-042259_create_users/up.sql
+++ migrations/2024-07-19-042259_create_users/up.sql
@@ -1,7 +1,8 @@
 -- Your SQL goes here
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR NOT NULL UNIQUE,
-                       password VARCHAR NOT NULL,
-                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR   NOT NULL UNIQUE,
+    password   VARCHAR   NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

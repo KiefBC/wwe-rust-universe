93d8b9f7c199a87daf41e79100b765ce8a0d9c8f
kiefer <kiefx@pm.me>
2024-07-22 20:05:07 -0700

    Refactor and extend tests in test_file.rs
    Refactored the existing tests for better readability and maintainability. Added a reset_test_user function for better handling of test user. Created new tests, namely test_check_user_exists and test_verify_credentials to further ensure the reliability of the functionality.

--- src-tauri/src/test_file.rs
+++ src-tauri/src/test_file.rs
@@ -1,49 +1,71 @@
-/**
-This file contains tests for the functions in the main.rs file
-Everything is sequential, so the tests will run in order
-This is a requirement for the create_user function, as it relies on the check_user_exists function
-*/
-
 #[cfg(test)]
 mod tests {
-    use crate::models::{ NewUser };
+    use crate::models::{NewUser};
     use crate::schema::users::dsl::*;
     use diesel::prelude::*;
-    use crate::db::{ create_user, check_user_exists, establish_connection };
+    use crate::db::{create_user, check_user_exists, establish_connection, verify_credentials};
+    use serial_test::serial;
 
-    /**
-    Testing the check_user_exists function
-    Do this first, as the create_user function relies on it
-    */
-    #[test]
-    fn test_check_user_exists() {
-        println!("Checking if user exists");
-        let conn = &mut establish_connection();
+    fn reset_test_user(conn: &mut PgConnection)  {
+        let test_user = NewUser { username: "Testing", password: "Testing" };
+        let result = diesel::delete(users.filter(username.eq(test_user.username)))
+            .filter(password.eq(test_user.password))
+            .execute(conn)
+            .expect("Error deleting test user");
 
-        // Check if the "Testing" user exists
-        assert_eq!(check_user_exists(conn, "Testing"), true);
-        assert_ne!(check_user_exists(conn, "Testing1"), true);
+        println!("Deleted {} user", result);
     }
 
-    /*
-    Testing the create_user function
-    It will first delete the "Testing" user if it exists, then create it
-     */
     #[test]
+    #[serial]
     fn test_create_user() {
         println!("Creating new user.... first deleting if it exists");
-        if check_user_exists(&mut establish_connection(), "Testing") {
-            let mut conn = establish_connection();
-            diesel::delete(users.filter(username.eq("Testing")))
-                .execute(&mut conn)
-                .expect("Error deleting user");
-        }
+        let mut conn = establish_connection();
+        reset_test_user(&mut conn);
 
         println!("Creating new user");
-        let mut conn = establish_connection();
         let new_user = NewUser { username: "Testing", password: "Testing" };
         let user = create_user(&mut conn, new_user);
+
         assert_eq!(user.username, "Testing");
         assert_ne!(user.username, "Testing1");
+    }
+
+    #[test]
+    #[serial]
+    fn test_check_user_exists() {
+        println!("Checking if user exists");
+        let mut conn = establish_connection();
+        reset_test_user(&mut conn);
+
+        let new_user = NewUser { username: "Testing", password: "Testing" };
+        create_user(&mut conn, new_user);
+
+        let username_check: &str = "Testing";
+        let username_check1: &str = "Testing1";
+
+        let result = check_user_exists(&mut conn, username_check);
+        let result1 = check_user_exists(&mut conn, username_check1);
+
+        assert_eq!(result, true);
+        assert_ne!(result1, true);
+    }
+
+    #[test]
+    #[serial]
+    fn test_verify_credentials() {
+        println!("Verifying credentials");
+        let mut conn = establish_connection();
+        reset_test_user(&mut conn);
+
+        let new_user = NewUser { username: "Testing", password: "Testing" };
+        create_user(&mut conn, new_user);
+
+        let username_check: &str = "Testing";
+        let password_check: &str = "Testing";
+        let password_check1: &str = "Testing1";
+
+        assert_eq!(verify_credentials(username_check.to_string(), password_check.to_string()), true);
+        assert_ne!(verify_credentials(username_check.to_string(), password_check1.to_string()), true);
     }
 }

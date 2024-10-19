f5120c68c532d258c84f154a3ddb08f5f9f5383b
kiefer <kiefx@pm.me>
2024-10-19 11:55:50 -0700

    Seperated /routes/login into Svelte Components based on learnings. Modified +page.svelte to use new components. Tidied up Typescript functionality.

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let src: string;
+</script>
+
+<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img class="mx-auto h-32 w-auto" src={src} alt="WWE Rust Universe">
+    <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-white">Placeholder</h2>
+</div>
